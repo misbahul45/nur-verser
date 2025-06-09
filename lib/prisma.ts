e85066d9095d676757e49e16f7b746a7af5f72d3
Misbahul Muttaqin <misbahulmuttaqin395@gmail.com>
2025-06-10 03:18:17 +0700

    prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,11 @@
+import { PrismaClient } from '@/lib/generated/prisma'
 
-import { PrismaClient } from '@prisma/client'
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+const globalForPrisma = global as unknown as { 
+    prisma: PrismaClient
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  log: ['query'],
-})
+const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
